f6910f80e8486b5a32fa50fb379f6b21fd38f186
Tim <leadmanager81@gmail.com>
2026-04-01 18:31:10 +0300

    chore: add .gitignore, remove dist and node_modules from git

--- dist/tools/geocode.d.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-export declare const geocodeSchema: z.ZodObject<{
-    address: z.ZodString;
-}, "strip", z.ZodTypeAny, {
-    address: string;
-}, {
-    address: string;
-}>;
-export declare function handleGeocode(params: z.infer<typeof geocodeSchema>): Promise<string>;
